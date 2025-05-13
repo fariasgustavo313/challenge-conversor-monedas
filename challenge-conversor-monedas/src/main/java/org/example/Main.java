02ae061fa759775c1f7cde2a196d38868435f12b
Gustavo Farias <ext_fargusta@mercadolibre.com>
2025-05-13 17:56:49 -0300

    obtener Exchange rate

--- challenge-conversor-monedas/src/main/java/org/example/Main.java
+++ challenge-conversor-monedas/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import com.google.gson.Gson;
 import org.example.api.ApiClient;
+import org.example.model.ExchangeRateResponse;
 
 import java.io.IOException;
 import java.net.http.HttpRequest;
@@ -12,10 +14,17 @@ public class Main {
         HttpRequest request = apiClient.createRequest();
 
         try {
-            String response = apiClient.getResponse(request);
-            System.out.println(response); // Probamos que todo funcione hasta acá
+            String json = apiClient.getResponse(request);
+            Gson gson = new Gson();
+            ExchangeRateResponse data = gson.fromJson(json, ExchangeRateResponse.class);
+
+            System.out.println("Moneda base: " + data.getBase_code());
+            System.out.println("Tasa ARS: " + data.getConversion_rates().get("ARS"));
+            System.out.println("Tasa BRL: " + data.getConversion_rates().get("BRL"));
+            System.out.println("Tasa CLP: " + data.getConversion_rates().get("CLP"));
+
         } catch (IOException | InterruptedException e) {
-            System.out.println("Error al conectar con la API: " + e.getMessage());
+            System.out.println("Error al obtener datos de la API: " + e.getMessage());
         }
     }
 }
